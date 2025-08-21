a6be65d1b00b3f1e3a69c236c9d92103a29184ec
ankit-027 <ankit2kk4@gmail.com>
2025-08-21 19:59:43 +0530

    Footer Dark

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,19 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
     return (
         <>
-            <footer className="py-3 my-4">
+            <footer className="py-3 mt-4 bg-dark text-light">
                 <ul className="nav justify-content-center border-bottom pb-3 mb-3">
                     <li className="nav-item">
-                        <Link to="/" className="nav-link px-2 text-body-secondary">Home</Link>
+                        <Link to="/" className="nav-link px-2">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/product" className="nav-link px-2 text-body-secondary">Product</Link>
+                        <Link to="/product" className="nav-link px-2">Product</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/movie" className="nav-link px-2 text-body-secondary">Movie</Link>
+                        <Link to="/movie" className="nav-link px-2">Movie</Link>
                     </li>
                 </ul>
-                <p className="text-center text-body-secondary">© 2025 Company, Inc</p>
+                <p className="text-center">© 2025 Company, Inc</p>
             </footer>
         </>
     )
